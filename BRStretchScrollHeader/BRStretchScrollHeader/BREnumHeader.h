759db4f315500dd488b08af5f8f1857e16438b90
gitBurning <974201360@qq.com>
2017-02-27 23:31:50 +0800

    修改移除kvo 崩溃

---
+++ BRStretchScrollHeader/BRStretchScrollHeader/BREnumHeader.h
@@ -0,0 +1,13 @@
+//
+//  BREnumHeader.h
+//  BRStretchScrollHeader
+//
+//  Created by gitBurning on 17/2/27.
+//  Copyright © 2017年 BR. All rights reserved.
+//
+
+#ifndef BREnumHeader_h
+#define BREnumHeader_h
+
+
+#endif /* BREnumHeader_h */
